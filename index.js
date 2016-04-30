63055d99eb970f048eb11fb59d3b864346ad38ef
Vansh Gandhi <vansh.gandhi@gmail.com>
2016-04-30 12:26:15 -0700

    Figuring out how to use Marvel's API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
+var http = require('http')
 var app = express()
 var base_url = "http://gateway.marvel.com/"
 var characters_base = "v1/public/characters/"
