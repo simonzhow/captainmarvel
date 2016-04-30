3ec1413bc6f6738447a28cb5e82c4827083ef671
Simon Zhou <simon.zhou260@gmail.com>
2016-04-30 10:39:04 -0400

    Postmates Integration

--- index.js
+++ index.js
@@ -500,3 +500,51 @@ function sendTextMessage(sender, text) {
     })
 }
 
+function hungry(sender)
+{
+
+    var delivery = {
+      pickup_address: "972 Gayley Ave, Los Angeles, CA",
+      dropoff_address: "407 Charles E Young Dr W, Los Angeles, CA"
+    };
+
+    postmates.quote(delivery, function(err, res) {
+    console.log(res.body.fee);  
+    sendTextMessage(sender, res.body.fee);
+});
+
+
+    var delivery = {
+        manifest: "a sandwhich",
+        pickup_name: "The Warehouse",
+        pickup_address: "20 McAllister St, San Francisco, CA",
+        pickup_phone_number: "555-555-5555",
+        pickup_business_name: "Optional Pickup Business Name, Inc.",
+        pickup_notes: "Optional note that this is Invoice #123",
+        dropoff_name: "Alice",
+        dropoff_address: "101 Market St, San Francisco, CA",
+        dropoff_phone_number: "415-555-1234",
+        dropoff_business_name: "Optional Dropoff Business Name, Inc.",
+        dropoff_notes: "Optional note to ring the bell",
+        quote_id: "qUdje83jhdk"
+    };
+ 
+    postmates.new(delivery, function(err, res) {
+        // `res.body` 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
