60c7346cf45756626068fcaba094ab1bc47f1223
Vansh Gandhi <vansh.gandhi@gmail.com>
2016-05-01 03:28:06 -0700

    Added new marvel api

---
+++ node_modules/marvel-api/spec/characters-spec.js
@@ -0,0 +1,658 @@
+var nock  = require('nock')
+  , util  = require('util')
+  , utils = require('../lib/utils')
+  , rootUrl = 'http://gateway.marvel.com'
+  , ts, hash;
+
+var characters = require('../lib/characters')({
+  publicKey: 'public-test'
+, privateKey: 'private-test'
+}, utils);
+
+utils.timestamp = function() {
+  return 1234;
+};
+
+utils.createHash = function() {
+  return '5678';
+};
+
+describe('characters', function() {
+
+  beforeEach(function() {
+    ts = utils.timestamp();
+    hash = utils.createHash(ts, 'private-test', 'public-test');
+  });
+
+  it('should call #findAll with the correct default parameters', function(done) {
+    var route = util.format(
+      '/v1/public/characters?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.findAll()
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #findAll with the correct limit', function(done) {
+    var route = util.format(
+      '/v1/public/characters?ts=%s&apikey=public-test&hash=%s&limit=10&offset=0'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.findAll(10)
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #findAll with the correct limit and offset', function(done) {
+    var route = util.format(
+      '/v1/public/characters?ts=%s&apikey=public-test&hash=%s&limit=10&offset=10'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.findAll(10, 10)
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #findNameStartsWith with a string parameter',function(done) {
+    var route = util.format(
+      '/v1/public/characters?ts=%s&apikey=public-test&hash=%s&offset=0&limit=20&nameStartsWith=Spi'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: {} } });
+
+    characters.findNameStartsWith('Spi')
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+
+  });
+
+  it('should call #findByName with the correct parameters', function(done) {
+    var route = util.format(
+      '/v1/public/characters?ts=%s&apikey=public-test&hash=%s&name=test-man'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: {} } });
+
+    characters.findByName('test-man')
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #find with the correct id', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s?ts=%s&apikey=public-test&hash=%s'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: {} } });
+
+    characters.find('1234')
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #comics with the correct default parameters', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s/comics?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.comics('1234')
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #comics with the correct limit', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s/comics?ts=%s&apikey=public-test&hash=%s&limit=10&offset=0'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.comics('1234', 10)
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #comics with the correct limit and offset', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s/comics?ts=%s&apikey=public-test&hash=%s&limit=10&offset=10'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.comics('1234', 10, 10)
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #events with the correct default parameters', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s/events?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.events('1234')
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #events with the correct limit', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s/events?ts=%s&apikey=public-test&hash=%s&limit=10&offset=0'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.events('1234', 10)
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #events with the correct limit and offset', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s/events?ts=%s&apikey=public-test&hash=%s&limit=10&offset=10'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.events('1234', 10, 10)
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #stories with the correct default parameters', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s/stories?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.stories('1234')
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #stories with the correct limit', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s/stories?ts=%s&apikey=public-test&hash=%s&limit=10&offset=0'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.stories('1234', 10)
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #stories with the correct limit and offset', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s/stories?ts=%s&apikey=public-test&hash=%s&limit=10&offset=10'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.stories('1234', 10, 10)
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #series with the correct default parameters', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s/series?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.series('1234')
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #series with the correct limit', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s/series?ts=%s&apikey=public-test&hash=%s&limit=10&offset=0'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.series('1234', 10)
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  it('should call #series with the correct limit and offset', function(done) {
+    var route = util.format(
+      '/v1/public/characters/%s/series?ts=%s&apikey=public-test&hash=%s&limit=10&offset=10'
+    , '1234'
+    , ts
+    , hash);
+
+    nock(rootUrl)
+      .get(route)
+      .reply(200, { data: { results: [] } });
+
+    characters.series('1234', 10, 10)
+      .then(function(data) {
+        data.should.exist;
+        done();
+      })
+      .done();
+  });
+
+  describe('error handling', function() {
+
+    it('should catch errors from #findAll', function(done) {
+      var route = util.format(
+        '/v1/public/characters?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(404, 'Not found');
+
+      characters.findAll()
+        .fail(function(err) {
+          err.should.exist;
+          err.message.should.equal('Not found');
+
+          done();
+        })
+        .done();
+    });
+
+    it('should catch errors from #findByName', function(done) {
+      var route = util.format(
+        '/v1/public/characters?ts=%s&apikey=public-test&hash=%s&name=test-man'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(404, 'Not found');
+
+      characters.findByName('test-man')
+        .fail(function(err) {
+          err.should.exist;
+          err.message.should.equal('Not found');
+
+          done();
+        })
+        .done();
+    });
+
+    it('should catch errors from #find', function(done) {
+      var route = util.format(
+        '/v1/public/characters/%s?ts=%s&apikey=public-test&hash=%s'
+      , '1234'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(404, 'Not found');
+
+      characters.find('1234')
+        .fail(function(err) {
+          err.should.exist;
+          err.message.should.equal('Not found');
+
+          done();
+        })
+        .done();
+    });
+
+    it('should catch errors from #comics', function(done) {
+      var route = util.format(
+        '/v1/public/characters/%s/comics?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+      , '1234'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(404, 'Not found');
+
+      characters.comics('1234')
+        .fail(function(err) {
+          err.should.exist;
+          err.message.should.equal('Not found');
+
+          done();
+        })
+        .done();
+    });
+
+    it('should catch errors from #events', function(done) {
+      var route = util.format(
+        '/v1/public/characters/%s/events?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+      , '1234'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(404, 'Not found');
+
+      characters.events('1234')
+        .fail(function(err) {
+          err.should.exist;
+          err.message.should.equal('Not found');
+
+          done();
+        })
+        .done();
+    });
+
+    it('should catch errors from #series', function(done) {
+      var route = util.format(
+        '/v1/public/characters/%s/series?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+      , '1234'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(404, 'Not found');
+
+      characters.series('1234')
+        .fail(function(err) {
+          err.should.exist;
+          err.message.should.equal('Not found');
+
+          done();
+        })
+        .done();
+    });
+
+    it('should catch errors from #stories', function(done) {
+      var route = util.format(
+        '/v1/public/characters/%s/stories?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+      , '1234'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(404, 'Not found');
+
+      characters.stories('1234')
+        .fail(function(err) {
+          err.should.exist;
+          err.message.should.equal('Not found');
+
+          done();
+        })
+        .done();
+    });
+  });
+
+  describe('optional parameters with callbacks', function() {
+
+    it('should call #findAll with the correct defaults', function(done) {
+      var route = util.format(
+        '/v1/public/characters?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(200, { data: { results: [] } });
+
+      characters.findAll(function(err, data) {
+        (err === null).should.equal(true);
+        data.should.exist;
+        done();
+      });
+    });
+
+    it('should call #findAll with the correct limit', function(done) {
+      var route = util.format(
+        '/v1/public/characters?ts=%s&apikey=public-test&hash=%s&limit=10&offset=0'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(200, { data: { results: [] } });
+
+      characters.findAll(10, function(err, data) {
+        (err === null).should.equal(true);
+        data.should.exist;
+
+        done();
+      });
+    });
+
+    it('should call #comics with the correct defaults', function(done) {
+      var route = util.format(
+        '/v1/public/characters/%s/comics?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+      , '1234'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(200, { data: { results: [] } });
+
+      characters.comics('1234', function(err, data) {
+        (err === null).should.equal(true);
+        data.should.exist;
+        done();
+      });
+    });
+
+    it('should call #comics with the correct limit', function(done) {
+      var route = util.format(
+        '/v1/public/characters/%s/comics?ts=%s&apikey=public-test&hash=%s&limit=10&offset=0'
+      , '1234'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(200, { data: { results: [] } });
+
+      characters.comics('1234', 10, function(err, data) {
+        (err === null).should.equal(true);
+        data.should.exist;
+        done();
+      });
+    });
+
+    it('should call #events with the correct defaults', function(done) {
+      var route = util.format(
+        '/v1/public/characters/%s/events?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+      , '1234'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(200, { data: { results: [] } });
+
+      characters.events('1234', function(err, data) {
+        (err === null).should.equal(true);
+        data.should.exist;
+        done();
+      });
+    });
+
+    it('should call #events with the correct limit', function(done) {
+      var route = util.format(
+        '/v1/public/characters/%s/events?ts=%s&apikey=public-test&hash=%s&limit=10&offset=0'
+      , '1234'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(200, { data: { results: [] } });
+
+      characters.events('1234', 10, function(err, data) {
+        (err === null).should.equal(true);
+        data.should.exist;
+        done();
+      });
+    });
+
+    it('should call #stories with the correct defaults', function(done) {
+      var route = util.format(
+        '/v1/public/characters/%s/stories?ts=%s&apikey=public-test&hash=%s&limit=20&offset=0'
+      , '1234'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(200, { data: { results: [] } });
+
+      characters.stories('1234', function(err, data) {
+        (err === null).should.equal(true);
+        data.should.exist;
+        done();
+      });
+    });
+
+    it('should call #stories with the correct limit', function(done) {
+      var route = util.format(
+        '/v1/public/characters/%s/stories?ts=%s&apikey=public-test&hash=%s&limit=10&offset=0'
+      , '1234'
+      , ts
+      , hash);
+
+      nock(rootUrl)
+        .get(route)
+        .reply(200, { data: { results: [] } });
+
+      characters.stories('1234', 10, function(err, data) {
+        (err === null).should.equal(true);
+        data.should.exist;
+        done();
+      });
+    });
+  });
+});
