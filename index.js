0c19ad10708d3dd8298eabd81f4df8c785914063
jaiakt <jaiakt@gmail.com>
2016-05-01 05:56:40 -0700

    simplified wit.ai

--- index.js
+++ index.js
@@ -67,37 +67,23 @@ function handleWitData(error, data) {
     } 
     console.log('Yay, got Wit.ai response: ' + JSON.stringify(data));
     var entities = data.outcomes[0].entities;
+    if (!_.has(entities, 'intent') || !_.has(entities, 'object')) {
+        unableToParse();
+        return;
+    }
+    var searchTerm = entities.object[0].value
     switch(entities.intent[0].value) {
     case "search_comic":
         var funcToRun = searchForComic
-        if (!_.has(entities, 'comic')) {
-            unableToParse();
-            return;
-        }
-        var searchTerm = entities.comic[0].value
         break;
     case "search_character":
         var funcToRun = searchForCharacter
-        if (!_.has(entities, 'character')) {
-            unableToParse();
-            return;
-        }
-        var searchTerm = entities.character[0].value
         break;
     case "search_event":
         var funcToRun = searchForEvent
-        if (!_.has(entities, 'event')) {
-            unableToParse();
-            return;
-        }
-        var searchTerm = entities.event[0].value
         break;
     case "search_generic":
         var funcToRun = searchForGeneric
-        if (!_.has(entities, 'generic')) {
-            unableToParse();
-            return;
-        }
         var searchTerm = entities.generic[0].value
         break;
     default:
