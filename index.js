e231aec4be13ff715ca82247b11b1d740d5b5c10
jaiakt <jaiakt@gmail.com>
2016-05-01 02:18:50 -0700

    fixed cb error

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post('/webhook/', function (req, res) {
         if (event.message && event.message.text) {
             text = event.message.text
 			const context = {};
-			client.message(text, context, (error, data) => {
+			client.message(text, context, function (error, data) {
 				if (error) {
 					// Wit was unsuccessful
 				} else {
